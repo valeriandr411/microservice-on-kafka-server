0874464413f7068c3d1d89303363dec5ffe52f45
PokhilenkoVV <pokhilenko@lanit.ru>
2024-05-24 14:16:54 +0300

    правки в проекте

--- src/main/java/org/example/kafka/Listeners.java
+++ src/main/java/org/example/kafka/Listeners.java
@@ -5,17 +5,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.example.model.Order;
+import org.example.service.ProductionService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+
 @Service
-@Slf4j
 public class Listeners {
     private final ObjectMapper objectMapper = new ObjectMapper();
-    @KafkaListener(topics = "completed", groupId = "app123")
-    public void msListener(ConsumerRecord<Long,Order> record) {
-        System.out.println("Заказ готов: " + record.value());
+    @KafkaListener(topics = "created", groupId = "app123")
+    public void msgListener(ConsumerRecord<Long,Order> record) throws InterruptedException, JsonProcessingException {
+        System.out.println("Создан заказ: " + record.value());
         System.out.println("----------------------------------------");
+        String order = String.valueOf(record.value());
+        Order order2 = objectMapper.readValue(order,Order.class);
+        ProductionService.cook(order2);
+        ProductionService.completed(order2);
     }
-
 }
---
+++ src/main/java/org/example/service/ProductionService.java
@@ -0,0 +1,45 @@
+package org.example.service;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.example.kafka.KafkaProducerConfig;
+import org.example.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductionService {
+    private static KafkaTemplate<Long, Order> kafkaTemplate ;
+    @Autowired
+    public ProductionService(KafkaTemplate<Long, Order> template){
+        kafkaTemplate = template;
+    }
+    public static Order cook(Order order) throws InterruptedException {
+        System.out.println(String.format("Заказа №%s начал готовиться...", order.getId()));
+        ProductionService.setStatus(order.getId(),"COOKING");
+        Thread.sleep(60_000);//ожидает 10 секунд
+        return order;
+    }
+    public static Order completed(Order order){
+        ProductionService.setStatus(order.getId(),"COMPLETED");
+        kafkaTemplate.send("completed", order.getId(), order);
+        return order;
+    }
+
+    public static void setStatus(long id, String status){
+        RequestSpecification requestSpecification = RestAssured.given()
+                .baseUri("http://0.0.0.0:8081")
+                .contentType(ContentType.JSON)
+                .basePath("/pizza/order/status")
+                .body(String.format("{\"id\":%d, \"status\":\"%s\"}", id, status));
+        Response response = requestSpecification
+                .post()
+                .then()
+                .statusCode(200)
+                .extract().response();
+    }
+
+}
